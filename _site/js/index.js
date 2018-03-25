436eae090f5ea3022b68f5da7c67f74825996815
Felix Susanto <felix.susanto2000@gmail.com>
2018-03-25 21:56:30 +0800

    adding menu interaction

--- _site/js/index.js
+++ _site/js/index.js
@@ -1,3 +1,9 @@
 $(document).ready(function () {
   $('.full-carousel').slick();
+  $('#main-nav').on('hide.bs.collapse', function() {
+    $(this).removeClass('white-overlay');
+  });
+  $('#main-nav').on('show.bs.collapse', function() {
+    $(this).addClass('white-overlay');
+  });
 });
